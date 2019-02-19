bfcce3296a6557738bd711cc1b949376547d7418
sandracmoore <47160836+sandracmoore@users.noreply.github.com>
2019-02-19 13:32:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,103 @@
-const appKey = "f066c960536fac70c08c761fd722a219";
+/* ------------------------
+Weather World Example
+This example will search Open Weather for a user submitted entry
+And then populate those results on the page using the jQuery 'append' function
+OpenWeather API - http://openweathermap.org/api
+Please sign up on OpenWeatherMap to get your own APPID.
+------------------------- */
 
-let searchButton = document.getElementById("search-btn");
-let searchInput = document.getElementById("search-txt");
-let cityName = document.getElementById("city-name");
-let icon = document.getElementById("icon");
-let temperature = document.getElementById("temp");
-let humidity = document.getElementById("humidity-div");
+//Define the url for the weather API call
+var weatherBaseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var weatherQueryParams = '&units=imperial&APPID=f066c960536fac70c08c761fd722a219';
 
-searchButton.addEventListener("click", findWeatherDetails);
-searchInput.addEventListener("keyup", enterPressed);
+//Create a function that will generate an HTML string 
+//And then add that string to the page
+function createHTML(cityName, tempValue){
+	var bgClass;
+	if (tempValue >= 90){
+		bgClass = 'redBg';
+	}
+	else if (tempValue < 90 && tempValue >= 80){
+		bgClass = 'orangeBg';
+	}
+	else if (tempValue < 80 && tempValue >= 70){
+		bgClass = 'yellowBg';
+	}
+	else if (tempValue < 70 && tempValue >= 60){
+		bgClass = 'greenBg';
+	}
+	else if (tempValue < 60 && tempValue >= 50){
+		bgClass = 'blueBg';
+	}
+	else{
+		bgClass = 'grayBg';
+	}
 
-function enterPressed(event) {
-  if (event.key === "Enter") {
-    findWeatherDetails();
-  }
+	var htmlString =	'<div class="setBorder ' + bgClass + '">' +
+											'<div class="weatherCity">' + cityName + '</div>' +
+											'<div class="weatherData">' + tempValue + '</div>' +
+										'</div>';
+	$('#weatherResults').prepend(htmlString);
 }
 
-function findWeatherDetails() {
-  if (searchInput.value === "") {
-  
-  }else {
-    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
-   httpRequestAsync(searchLink, theResponse);
-  }
- }
+//Create a function that will execute the Weather AJAX call
+var searchWeather = function(city){
 
-function theResponse(response) {
-  let jsonObject = JSON.parse(response);
-  cityName.innerHTML = jsonObject.name;
-  icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-  temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
-  humidity.innerHTML = jsonObject.main.humidity + "%";
-}
+	var searchURL = weatherBaseURL + city + weatherQueryParams;
+	$.ajax({
+		url: searchURL,
+		type: 'GET',
+		dataType: 'json',
+		error: function(data){
+			console.log("We got problems");
+			console.log(data.status);
+			alert("Oh no. Something went wrong...");
+		},
+		success: function(data){
+			console.log("WooHoo!");
+			//Check the browser console to see the returned data
+			console.log(data);
+			//Check to make sure the success response is ok
+			if (data.cod === '404'){
+				alert("Oh no. Something went wrong. Try another city");
+				//adding a return will end the success function
+				return;
+			}
 
-function httpRequestAsync(url, callback)
-{
-  console.log("hello");
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => { 
-        if (httpRequest.readyState == 4 && httpRequest.status == 200)
-            callback(httpRequest.responseText);
-    }
-    httpRequest.open("GET", url, true); // true for asynchronous 
-    httpRequest.send();
-}
+			$("#query").val('');
+
+			var theCity = data.name || '????';
+			var theTemp = Math.round(data.main.temp) || 70;
+
+			//Call a function that will create an HTML string & add it to the page
+			createHTML(theCity, theTemp);
+		}
+	});
+};
+
+//Code to be executed once the page has fully loaded
+$(document).ready(function(){
+	console.log("LOADED!!!!");
+
+	//Use jQuery to assign a (callback) function to occur when the 'search' button is clicked
+	$("#search").on('click', function(){
+		console.log("Clicked search");
+		//Use jQuery to get the value of the 'query' input box
+		var newSearchTerm = $("#query").val();
+		console.log(newSearchTerm);
+		//Execute the Weather API call with the 'newSearchTerm' string as its argument 
+		searchWeather(newSearchTerm);
+		$("#search").blur();
+	});
+
+	//What if someone just wants to click "ENTER"???
+	//Use jQuery to assign a (callback) function to occur when enter is pressed 
+	//This will ONLY work when the '#query' input box is active
+	$('#query').on('keypress', function(e){
+		//If enter key is pressed
+		if (e.which == 13){
+			//Use jQuery's trigger() function to execute a click event on the '#search' element
+			$("#search").trigger('click');
+		}
+	});
+});
